d03f8f0d32928c13977aa56aa81eeedbc3a3a192
Sajaabualfoul <sajafirst@yahoo.com>
2018-04-17 14:27:29 -0500

    Add files via upload
    i'm dumb

--- MPCNC813_GLCD/Configuration.h
+++ MPCNC813_GLCD/Configuration.h
@@ -1,5 +1,5 @@
 /**
- * Marlin 3D Printer Firmware
+ * Marlin 3D Printer I'm adding this crap Firmware
  * Copyright (C) 2016 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
  *
  * Based on Sprinter and grbl.
